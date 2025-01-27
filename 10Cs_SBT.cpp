3a3801a7573e0afb9c695ae1d7d8c5d83c981fe7
sbeckfor10 <141409128+sbeckfor10@users.noreply.github.com>
2025-01-27 16:32:46 +0000

    Add files via upload

---
+++ 10Cs_SBT.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "An In Silico Reformulation of the Ten Commandments for the 2000s\n";
+  std::cout << " \n";
+  std::cout << "1. There is only one God. His name is Jehovah.\n";
+  std::cout << "2. Do not worship other Gods.\n";
+  std::cout << "3. Do not swear.\n";
+  std::cout << "4. Remember to work and rest.\n";
+  std::cout << "5. Look after your parents whilst protecting yourself.\n";
+  std::cout << "6. Do not kill except in self-defence.\n";
+  std::cout << "7. Do not cheat on your boyfriend, girlfriend, wife or husband.\n";
+  std::cout << "8. Do not steal.\n";
+  std::cout << "9. Do not lie.\n";
+  std::cout << "10. Do not say or write anything negative about someone in case you get in trouble for defamation. The exception to the rule is when making a formal complaint.\n";
+  std::cout << "11. Do not be jealous.\n";
+  std::cout << "12. Safe and moderate pornography use is acceptable if you are involuntarily single. But marriage and procreation should be aspired to rather than porn use. \n";
+  std::cout << "13. Sex with different partners before marriage should be minimal to reduce the risk of STDs.\n";
+  std::cout << "14. The brain is the most sacred organ and should be spoken to, not medicated or experimented on.\n";
+  std::cout << "15. Competition should be fair and ethical. \n";
+  std::cout << "16. People should be allowed to use their free will and make decisions even if they are the wrong choices.\n";
+
+
+  //by Simeon Beckford-Tongs BSc (Hons), MSc
+  
+}
